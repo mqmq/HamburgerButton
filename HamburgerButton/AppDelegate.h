4095557c2716c073a1e3330239ff60bb61cbc7c0
danis <danisfabric@gmail.com>
2014-11-15 13:16:36 +0800

    Initial Commit

---
+++ HamburgerButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HamburgerButton
+//
+//  Created by Danis on 14/11/15.
+//  Copyright (c) 2014年 danis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HamburgerButton/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HamburgerButton
+//
+//  Created by Danis on 14/11/15.
+//  Copyright (c) 2014年 danis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
